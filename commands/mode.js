e9424c7d58bb85f3b944dd5af6b7512981d32c92
whiskers75 <whiskers75@whiskers75.co.uk>
2014-05-06 18:35:03 +0100

    kick() and mode() improvements

--- commands/mode.js
+++ commands/mode.js
@@ -1,21 +1,12 @@
 module.exports.fn = function(global, mode, target) {
     var p = global.Q.defer();
-    global.m.collection('users').find({
-        host: global.raw.host,
-        user: global.raw.user
-    }).toArray(function(err, results) {
-        if (err) return p.reject(err.message);
-        if (!results[0][global.to]) {
-            p.reject('No permissions.');
-            return;
-        }
-        if (results[0][global.to].indexOf('mode') == -1) {
-            p.reject('No permissions.');
-            return;
-        }
-        global.run("w.send('MODE', '" + global.to + "', '" + mode + "'" + (target ? ", '" + target + "')" : ")"));
-        p.resolve();
-    });
+    var esc = require('js-string-escape');
+    if (global.user.indexOf('chanop') == -1) {
+        p.reject('You must have the "chanop" permission to do that.');
+        return;
+    }
+    global.run("w.send('MODE', '" + esc(global.to) + "', '" + esc(mode) + "'" + (target ? ", '" + esc(target) + "')" : ")"));
+    p.resolve();
     return p.promise;
 };
 module.exports.level = 1;
