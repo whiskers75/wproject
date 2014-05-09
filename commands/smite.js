66cf5d769abc7345289461ddf001afa0c4b4a5c8
whiskers75 <whiskers75@whiskers75.co.uk>
2014-05-09 19:23:45 +0100

    pls2not\n

--- commands/smite.js
+++ commands/smite.js
@@ -5,7 +5,7 @@ module.exports.fn = function(global, victim) {
         host: global.raw.host,
         user: global.raw.user
     }).toArray(function(err, results) {
-        global.run('w.action("' + global.to + '", "smites ' + esc(String(victim)) + ' with his +5,+5 quarterstaff of chaos!")');
+        global.run('w.action("' + global.to + '", "smites ' + esc(String(victim)).replace(/[\r\n\v\f\x85\u2028\u2029]/g, '') + ' with his +5,+5 quarterstaff of chaos!")');
         p.resolve();
     });
     return p.promise;
