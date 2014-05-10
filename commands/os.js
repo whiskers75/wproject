03e0f6c8e593705efe4b14eb2a208be60b0fb132
whiskers75 <whiskers75@whiskers75.co.uk>
2014-05-10 15:03:19 +0100

    OH FOR CRYING OUT LOUD NYU

--- commands/os.js
+++ commands/os.js
@@ -1,5 +1,11 @@
 module.exports.fn = function(global) {
     var p = global.Q.defer();
+    if (typeof global.slapped != 'undefined' && global.slapped > 2 && global.user.level < 10) {
+        p.reject('pls2notspam');
+        return p.promise;
+    }
+    if (typeof global.slapped == 'undefined') global.slapped = 0;
+    global.slapped++;
     global.reply('^w running on ' + require('os').hostname() + ' - load average (1m): ' + require('os').loadavg()[0].toFixed(2));
     p.resolve();
     return p.promise;
